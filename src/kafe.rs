1cde990dcfdf67fd8c8e341dc04d5679ba1eb1c0
valiksinev <valiksinev@yahoo.com>
2023-12-04 01:46:37 +0400

    fix kafe

--- src/kafe.rs
+++ src/kafe.rs
@@ -6,6 +6,12 @@ pub fn execute(){
     io::stdin().read_line(&mut s).unwrap();
     let n: usize = s.trim().parse().unwrap();
 
+    if n == 0 {
+        println!("0");
+        println!("0 0");
+        return;
+    }
+
     let mut price_list = vec![0;n];
     for i in 0..n {
         s.clear();
@@ -17,7 +23,15 @@ pub fn execute(){
     let mut g = vec![vec![u32::MAX; n+1];n];
 
     let mut coupon_count = 0;
+
+    let mut k2 = vec![0_usize; n+1];
+    let mut k2_days = vec![vec![]; n+1];
+
+    let mut k2_cur = k2.clone();
+    let mut k2_days_cur = k2_days.clone();
+
     for day in 0..n {
+
         let price = price_list[day];
         if price > 100 {
             coupon_count += 1;
@@ -25,10 +39,26 @@ pub fn execute(){
                 g[0][coupon_count] = price;
             } else {
                 for coupon in 0..coupon_count+1 {
-                    g[day][coupon] = if coupon == 0 {
-                        g[day-1][coupon +1]
+                    if coupon == 0 {
+                        if g[day-1][coupon +1] != u32::MAX {
+                            g[day][coupon] = g[day-1][coupon +1];
+
+                            k2_cur[coupon] = k2_cur[coupon + 1] + 1;
+                            k2_days_cur[coupon] = k2_days[coupon + 1].clone();
+                            k2_days_cur[coupon].push(day)
+                        }
                     } else {
-                        (g[day-1][coupon-1].saturating_add(price)).min(g[day-1][coupon+1])
+                        let cash = g[day-1][coupon-1].saturating_add(price);
+                        let free = g[day-1][coupon+1];
+                        if cash < free {
+                            g[day][coupon] = cash;
+                        } else {
+                            g[day][coupon] = free;
+
+                            k2_cur[coupon] = k2_cur[coupon + 1] + 1;
+                            k2_days_cur[coupon] = k2_days[coupon + 1].clone();
+                            k2_days_cur[coupon].push(day)
+                        }
                     };
                 }
             }
@@ -37,11 +67,35 @@ pub fn execute(){
                 g[0][0] = price;
             } else {
                 for coupon in 0..coupon_count+1 {
-                    g[day][coupon] = (g[day-1][coupon].saturating_add(price)).min(g[day-1][coupon+1])
+                    let cash = g[day-1][coupon].saturating_add(price);
+                    let free = g[day-1][coupon+1];
+                    if cash < free {
+                        g[day][coupon] = cash;
+                    } else {
+                        g[day][coupon] = free;
+
+                        k2_cur[coupon] = k2_cur[coupon + 1] + 1;
+                        k2_days_cur[coupon] = k2_days[coupon + 1].clone();
+                        k2_days_cur[coupon].push(day)
+                    }
                 }
             }
         }
+        k2 = k2_cur.clone();
+        k2_days = k2_days_cur.clone();
     }
-    println!("{:?}", g[n-1][0]);
 
+    let mut res = u32::MAX;
+    let mut k1 = 0;
+    for i in 0..coupon_count+1 {
+        if g[n-1][i] <= res {
+            res = g[n-1][i];
+            k1 = i;
+        }
+    }
+
+    let k2 = k2[k1];
+    println!("{:?}", res);
+    println!("{} {:?}", k1, k2);
+    k2_days[k1].iter().for_each(|&a| print!("{} ", &a+1));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-mod nails;
+// mod nails;
 // mod cheapest_way;
 // mod most_expensive_way;
 // mod breadth_first_search;
@@ -6,14 +6,14 @@ mod nails;
 // mod bubble_sort;
 // mod binary_search;
 // mod quick_sort;
-// mod kafe;
+mod kafe;
 // mod needleman_wunsch;
 // mod linked_components;
 // mod cheating;
 // mod topsort;
 
 fn main() {
-    nails::execute();
+    // nails::execute();
     // cheapest_way::execute();
     // most_expensive_way::execute();
     // breadth_first_search::execute();
@@ -21,7 +21,7 @@ fn main() {
     // bubble_sort::execute();
     // binary_search::execute();
     // quick_sort::execute();
-    // kafe::execute();
+    kafe::execute();
     // needleman_wunsch::execute();
     // linked_components::execute();
     // cheating::execute();
