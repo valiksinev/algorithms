38b6bcaa187e67c36e0260b5f551a35ccd103e44
valiksinev <valiksinev@yahoo.com>
2023-12-05 00:51:18 +0400

    refactor cafe

--- src/kafe.rs
+++ src/kafe.rs
@@ -12,96 +12,91 @@ pub fn execute(){
         return;
     }
 
-    let mut price_list = vec![0;n];
+    let mut cost = vec![0;n];
     for i in 0..n {
         s.clear();
         io::stdin().read_line(&mut s).unwrap();
         let price : u32 = s.trim().parse().unwrap();
-        price_list[i] = price;
+        cost[i] = price;
     }
 
-    let mut g = vec![vec![u32::MAX; n+1];n];
+    let mut g: Vec<Vec<Option<(u32, Vec<u32>)>>> = vec![vec![None; n+1];n];
 
-    let mut coupon_count = 0;
+    let (result, k1, days) = {
+        let mut min = u32::MAX;
+        let mut k1 = 0;
+        let mut v = vec![];
+        for i in 0..=n {
+            let (val, vec) = dp(n-1, i, &mut cost, &mut g);
+            if min >= val {
+                min = val;
+                k1 = i;
+                v = vec;
+            }
+        }
+        (min, k1, v)
+    };
 
-    let mut k2 = vec![0_usize; n+1];
-    let mut k2_days = vec![vec![]; n+1];
+    println!("{:?}", result);
+    println!("{} {:?}", k1, days.len());
+    days.iter().for_each(|&a| print!("{} ", &a+1));
+}
 
-    let mut k2_cur = k2.clone();
-    let mut k2_days_cur = k2_days.clone();
-
-    for day in 0..n {
-
-        let price = price_list[day];
-        if price > 100 {
-            coupon_count += 1;
-            if day == 0 {
-                g[0][coupon_count] = price;
+fn dp (day: usize, j: usize, cost: &[u32], g: &mut Vec<Vec<Option<(u32, Vec<u32>)>>>) -> (u32, Vec<u32>) {
+    if j.saturating_sub(day)  > 1 {
+        (u32::MAX, vec![])
+    } else if day == 0 {
+        let val = if j == 0 {
+            if cost[day] > 100 {
+                u32::MAX
             } else {
-                for coupon in 0..coupon_count+1 {
-                    if coupon == 0 {
-                        if g[day-1][coupon +1] != u32::MAX {
-                            g[day][coupon] = g[day-1][coupon +1];
-
-                            k2_cur[coupon] = k2[coupon + 1] + 1;
-                            k2_days_cur[coupon] = k2_days[coupon + 1].clone();
-                            k2_days_cur[coupon].push(day)
-                        }
-                    } else {
-                        let cash = g[day-1][coupon-1].saturating_add(price);
-                        let free = g[day-1][coupon+1];
-                        if cash < free {
-                            g[day][coupon] = cash;
-                        } else {
-                            g[day][coupon] = free;
-                            if g[day-1][coupon +1] != u32::MAX {
-                                k2_cur[coupon] = k2[coupon + 1] + 1;
-                                k2_days_cur[coupon] = k2_days[coupon + 1].clone();
-                                k2_days_cur[coupon].push(day)
-                            }
-
-
-                        }
-                    };
-                }
+                cost[day]
             }
         } else {
-            if day == 0 {
-                g[0][0] = price;
+            if cost[day] > 100 {
+                cost[day]
             } else {
-                for coupon in 0..coupon_count+1 {
-                    let cash = g[day-1][coupon].saturating_add(price);
-                    let free = g[day-1][coupon+1];
-                    if cash < free {
-                        g[day][coupon] = cash;
+                u32::MAX
+            }
+        };
+        (val, vec![])
+    } else {
+        if let Some((val,vec)) = g[day][j].clone() {
+            (val, vec)
+        } else {
+            if j == 0 {
+                if cost[day] > 100 {
+                    let (val, mut vec) =  dp(day-1, j+1, cost, g);
+                    vec.push(day as u32);
+                    (val, vec)
+                } else {
+                    let (mut a, vec_a) = dp(day-1, j, cost, g);
+                    a = a.saturating_add(cost[day]);
+                    let (b, mut vec_b) = dp(day-1, j+1, cost, g);
+                    if a < b {
+                        (a, vec_a)
                     } else {
-                        g[day][coupon] = free;
-
-                        if g[day-1][coupon +1] != u32::MAX {
-                            k2_cur[coupon] = k2[coupon + 1] + 1;
-                            k2_days_cur[coupon] = k2_days[coupon + 1].clone();
-                            k2_days_cur[coupon].push(day)
-
-                        }
+                        vec_b.push(day as u32);
+                        (b, vec_b)
                     }
                 }
+            } else {
+                let (mut a, vec_a) = if cost[day] > 100 {
+                    dp(day-1, j-1, cost, g)
+                } else {
+                    dp(day-1, j, cost, g)
+                };
+                a = a.saturating_add(cost[day]);
+                let (b, mut vec_b) = dp(day-1, j+1, cost, g);
+                let res = if a < b {
+                    (a, vec_a)
+                } else {
+                    vec_b.push(day as u32);
+                    (b, vec_b)
+                };
+                g[day][j] = Some(res);
+                g[day][j].clone().unwrap()
             }
         }
-        k2 = k2_cur.clone();
-        k2_days = k2_days_cur.clone();
     }
-
-    let mut res = u32::MAX;
-    let mut k1 = 0;
-    for i in 0..coupon_count+1 {
-        if g[n-1][i] <= res {
-            res = g[n-1][i];
-            k1 = i;
-        }
-    }
-
-    let k2 = k2[k1];
-    println!("{:?}", res);
-    println!("{} {:?}", k1, k2);
-    k2_days[k1].iter().for_each(|&a| print!("{} ", &a+1));
 }
